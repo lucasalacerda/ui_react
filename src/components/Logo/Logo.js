9da09980d3be7a3281bb81acefd90386f580a732
Lucas Lacerda <lucasalvlacerda@gmail.com>
2018-06-03 01:22:15 -0300

    add componente de logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,14 +4,7 @@ import './index.css';
 export class Logo extends React.Component {
     render() {
         return (
-            <ul className="nav">
-                <li className="nav-item">
-                    <a className="nav-link active" href="#id">Active</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#item">Link</a>
-                </li>
-            </ul>
+           <h1>Titulo do projeto</h1>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { SearchInput } from './components/Input/SearchInput';
 import { Header } from "./components/Header/Header";
+import { Logo } from "./components/Logo/Logo";
 
 class Body extends React.Component {
     render() {
         return[
             <Header />,
+            <Logo />,
             <SearchInput propTest="lala" fieldPlaceholder="Search" buttonTitle="lalala"/>
         ]
     }
