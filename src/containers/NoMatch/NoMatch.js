c0bdf95531fc01d6339e2986f852a627d924632b
lucasalacerda <lucasalvlacerda@gmail.com>
2018-06-07 21:28:39 -0300

    Cria container que serão componentes de páginas; cria rotas para renderizar estes componentes

---
+++ src/containers/NoMatch/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class NoMatch extends React.Component {
+    render(){
+        return(
+            <p>Ops! This page doessn't exist :(</p>
+        )
+    }
+}
