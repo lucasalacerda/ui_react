9157c9d57abf956191a0023fb02916d5844f44b6
Lucas <lucasalvlacerda@gmail.com>
2018-07-09 22:01:44 -0300

    Remove stuff

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import FormSearch from '../../components/FormSearch/FormSearch';
 import Title from '../../components/Title/Title';
 import ResultList from '../../components/ResultList/ResultList';
 import Loading from '../../components/Loading/Loading';
 import Input from '../../components/Input/Input';
-// import Carousel from '../../components/HomeComponents/Carousel/Carousel';
-
-// const API_KEY_GOOGLE = 'AIzaSyAoc_tIcnfMn5ey_Fjxm5pqTPfBcuqPPVA';
-// const API_KEY_MARVEL = '6dc70d3e96ba2c968b203274f0ceab4c';
 
 class Home extends React.Component {
 
@@ -23,17 +18,6 @@ class Home extends React.Component {
 		})
 		console.log(value)
 	}
-
-  handleSubmit = async (e) => {			
-
-		e.preventDefault();
-		const api_call = await fetch(`https://api.jikan.moe/search/anime?q=${this.state.filter}&page=1`);
-		const data = await api_call.json();
-					const locals = data;
-  	  this.setState({
-				locals
-			});
-	}
 	
 	componentDidMount = async () => {
 		const myHeaders = new Headers();
@@ -48,7 +32,6 @@ class Home extends React.Component {
 			headers: myHeaders,
 			mode: 'cors',
 			});
-		// const api_call = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${filter}&inputtype=textquery&fields=photos,formatted_address,filter,rating,opening_hours,geometry&key=${API_KEY}`);
 		const data = await api_call.json();
 		const locals = data;
 		console.log(locals)
@@ -77,12 +60,6 @@ class Home extends React.Component {
       <div>
         <Title title="Events"/>
 				<Input onChange={this.handleChange} type="text" name="filter" fieldPlaceholder="filter for events, countries, cities..."/>
-        {/* <FormSearch
-					onSubmit={this.handleSubmit} 
-					onChange={this.handleChange}
-        	fieldPlaceholder="filter for events, countries, cities..." 
-					title="filter"					
-					/>   */}
 				{result}
       </div>
     )
